ce6e82da450a943b35b0f66cfe1d880ef2a76b35
DashaDerpach <zipz231_ddv@student.ztu.edu.ua>
2025-01-30 00:38:01 +0200

    додано консольний проєкт, який рахує суму двох чисел

---
+++ project/project/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SumCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("number 1");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("number 2");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+
+            double sum = number1 + number2;
+
+            Console.WriteLine($"{number1}+{number2}={sum}");
+            Console.ReadKey();
+        }
+    }
+}
